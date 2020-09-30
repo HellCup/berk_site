abcadde58af044f3a9e9ca06e2be43bd6b1556bb
HellCup <69991456+HellCup@users.noreply.github.com>
2020-09-30 23:01:18 +0300

    Add files via upload

---
+++ config/connect.php
@@ -0,0 +1,10 @@
+<?php
+//Данные для подключения к БД
+    $user = 'mysql';
+    $password = "mysql";
+    $db = 'data_berkut';
+    $host = 'berkut';
+    //connect to DB
+    $conn = mysqli_connect($host, $user, $pass, $db);
+
+?>
