4e0591e85c3b6edb47e247f77338cdc455ffb57e
HellCup <69991456+HellCup@users.noreply.github.com>
2020-09-30 23:00:51 +0300

    Delete functions.php

--- mysql/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getLine($table, $value) {
-    global $link;
-    if (!$result = mysqli_query($link, "SELECT * FROM `$table` WHERE `id` like '$value'")){
-        die ("Ошибка вывода: ". mysqli_errno($link)." и " . mysqli_error($link));
-    } 
-    return mysqli_fetch_assoc($result);
-    }
-?>
